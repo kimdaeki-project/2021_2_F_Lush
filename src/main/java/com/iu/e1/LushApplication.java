770439789b87c1b44d05bf319070e170421943aa
Hyunmin Choi <74778827+hymn9611@users.noreply.github.com>
2021-11-12 17:21:05 +0900

    Initial commit

---
+++ src/main/java/com/iu/e1/LushApplication.java
@@ -0,0 +1,13 @@
+package com.iu.e1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LushApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iu/e1/LushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iu.e1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
